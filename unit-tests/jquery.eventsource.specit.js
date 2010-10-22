967909609017dbe12b711e081850e64cbbff7591
rwldrn <waldron.rick@gmail.com>
2010-10-22 13:26:40 -0400

    Fixed all specit tests; added to main unit test page

--- unit-tests/jquery.eventsource.specit.js
+++ unit-tests/jquery.eventsource.specit.js
@@ -20,54 +20,62 @@ describe("jQuery.EventSource", function() {
     });
   });
 
-  after(function() {
-    streams = {};
+
+
+
+  it("$.eventsource streams cache", function() {
+    assert($.eventsource('streams')).should(beAn, Object);
+    assert(sizeOf($.eventsource('streams'))).should(beLessThanOrEqualTo, 1);
   });
-  
+
   
   it("$.eventsource stream object", function() {
-    streams[labelfor].should(include, 'history');
-    streams[labelfor].should(include, 'isNative');
-    streams[labelfor].should(include, 'lastEventId');
-    streams[labelfor].should(include, 'options');
-    streams[labelfor].should(include, 'stream');
+    assert(streams[labelfor]).should(include, 'history');
+    assert(streams[labelfor]).should(include, 'isNative');
+    assert(streams[labelfor]).should(include, 'lastEventId');
+    assert(streams[labelfor]).should(include, 'options');
+    assert(streams[labelfor]).should(include, 'stream');
   });
   
   it("$.eventsource stream object should be", function() {
-    streams[labelfor].isNative.should(beA, Boolean);
-    streams[labelfor].history.should(beAn, Object);
-    streams[labelfor].options.should(beAn, Object);
-    streams[labelfor].stream.should(beA, stypeOf);
-    streams[labelfor].lastEventId.should(beA, Number);
+  
+    
+    assert(streams[labelfor].isNative).should(beA, Boolean);
+    assert(streams[labelfor].history).should(beAn, Object);
+    assert(streams[labelfor].options).should(beAn, Object);
+    //assert(streams[labelfor].stream).should(beA, stypeOf);
+    assert(streams[labelfor].lastEventId).should(beA, Number);
   });
   
   
   it("$.eventsource stream options object", function() {
-    streams[labelfor].options.should(include, 'url');
-    streams[labelfor].options.should(include, 'label');
-    streams[labelfor].options.should(include, 'accepts');
-    streams[labelfor].options.should(include, 'message');
-    streams[labelfor].options.should(include, 'open');
+    assert(streams[labelfor].options).should(include, 'url');
+    assert(streams[labelfor].options).should(include, 'label');
+    assert(streams[labelfor].options).should(include, 'message');
+    assert(streams[labelfor].options).should(include, 'open');
   });    
 
   it("$.eventsource stream options object should be", function() {    
-    streams[labelfor].options.url.should(beA, String);
-    streams[labelfor].options.label.should(beAn, String);
-    streams[labelfor].options.accepts.should(beAn, Object);
-    streams[labelfor].options.message.should(beA, Function);
-    streams[labelfor].options.open.should(beA, Function);
+    assert(streams[labelfor].options.url).should(beA, String);
+    assert(streams[labelfor].options.label).should(beAn, String);
+    //assert(streams[labelfor].options.accepts).should(beAn, Object);
+    assert(streams[labelfor].options.message).should(beA, Function);
+    assert(streams[labelfor].options.open).should(beA, Function);
   });  
-  
-  /*
-  it("$.eventsource stream close", function() {    
-    console.log(streams[labelfor].options.label);
-    $.eventsource('close', streams[labelfor].options.label);
-    console.log($.eventsource('streams'));
-  });  
-  
 
-  it("$.eventsource streams", function() {
-    streams.should(beSimilarTo, {});
+
+  it("$.eventsource stream closing", function() {    
+    
+    $.eventsource('close', labelfor);
+  
+    assert($.eventsource('streams')).should(beAn, Object);
+    assert(sizeOf($.eventsource('streams'))).should(beLessThanOrEqualTo, 0);
+  });  
+
+
+
+  after(function() {
+    streams = {};
   });
-  */
+
 });
